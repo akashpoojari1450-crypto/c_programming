7877954c09593fddd56fc83b546d7d815e00e6f2
Akash S <akashpoojari1450@gmail.com>
2026-05-12 12:36:02 +0000

    Fixed file path and moved scan_prc_char to input folder

---
+++ input/scan_pract_char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: \n");
+  
+    // Reads an Character
+    scanf("%c", &ch);  
+  
+    // Prints the Character
+    printf("Entered character is: %c\n", ch);  
+    return 0;
+}
