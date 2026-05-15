d4a441cdd771f114f4b26f3fdc2cbebaa7487179
Akash S <akashpoojari1450@gmail.com>
2026-05-15 16:51:40 +0000

    this is the code for operator

---
+++ input/operators/pract1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the number:\n");
+    scanf("%d %d",&a,&b);
+    printf("Addition is:%d\n",a+b);
+     printf("Subtarction result is:%d\n",a-b);
+       printf("Multiplication result is:%d\n",a*b);
+         printf("Division result is:%d\n",a/b);
+           printf("Modulus result is:%d",a%b);
+           return 0;
+}
