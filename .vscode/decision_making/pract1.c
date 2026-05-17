508e818008481c3d073102c8d93b53bf693f73c9
Akash S <akashpoojari1450@gmail.com>
2026-05-17 07:47:35 +0000

    first practice

---
+++ .vscode/decision_making/pract1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter  the number:");
+    scanf("%d",&num);
+    if(num>50)
+    {
+        printf("Agree");
+    }
+    else{
+        printf("ok");
+    }
+    return 0;
+}
