6b2b2e54859526cea92686252b14cf27fd1c4810
Akash S <akashpoojari1450@gmail.com>
2026-05-20 15:31:41 +0000

    using nested

---
+++ loops/nested1.c
@@ -0,0 +1,16 @@
+//we get output like i=0,j=0,i=1,j=1,i=2,j=2,i=3,j=3,i=4,j=4,
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    
+    for(j=0;j<n;j++)
+        {
+            printf("i:%d,j=%d\n",i,j);
+        
+        }
+    
+}
