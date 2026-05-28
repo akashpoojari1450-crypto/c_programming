c85e8243b1e5624c717ec7e0c54832003b85c86b
Akash S <akashpoojari1450@gmail.com>
2026-05-28 14:38:07 +0000

    global scope function:

---
+++ functions/global_scope.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int global=5;
+void disp()
+{
+    printf("%d",global);
+}
+int main()
+{
+    printf("Before interchanging:");
+    disp();
+
+    printf("After interchanging:");
+    global=10;
+    disp();
+    return 0;
+
+}
