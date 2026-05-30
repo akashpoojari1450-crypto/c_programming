48a0d4a913760860038655ba6521b3f91591754e
Akash S <akashpoojari1450@gmail.com>
2026-05-30 11:27:22 +0000

    tree recursion

---
+++ functions/tree_recursion.c
@@ -0,0 +1,16 @@
+//tree recursion
+#include<stdio.h>
+int tree(int n)
+{
+    if(n==0)
+    return;
+    printf("%d",n);
+    tree(n-1);
+
+    tree(n-1);
+}
+int main()
+{
+    tree(3);
+    return 0;
+}
