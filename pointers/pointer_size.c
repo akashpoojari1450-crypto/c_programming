cf32f61902c9ae36f8337c503f21f4800abd5110
Akash S <akashpoojari1450@gmail.com>
2026-05-31 07:17:37 +0000

    pointer size

---
+++ pointers/pointer_size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int *ptr;
+    char *ptr2;
+    printf("Size of int is %zu\n",sizeof(ptr));
+    printf("Size of char is %zu\n",sizeof(ptr2));
+    return 0;
+}
