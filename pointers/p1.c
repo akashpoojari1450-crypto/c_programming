152acefd119bebe2f94d12afc150a4475efdda44
Akash S <akashpoojari1450@gmail.com>
2026-05-31 07:23:08 +0000

    size of datatype using pointers

---
+++ pointers/p1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int *ptr;
+    char *ptr2;
+    printf("Size of int is %zu\n",sizeof(int));
+    printf("Size of char is %zu\n",sizeof(char));
+    return 0;
+}
